08919d359948b7025417eee46a4e462ee1b46b74
Andrea S Barr <andreabarr87@gmail.com>
2019-10-28 22:58:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  phrase = "It's-a me, Mario!"
+  phrase = 'It\'s-a me, Mario!'
   puts phrase
 end
 
@@ -9,4 +9,13 @@ def toadstool
 end
 
 def link
-  warning = ""
+  warning = 'It\'s Dangerous To Go Alone! Take This.'
+  puts warning
+end
+
+$video_game = "Mario Bros"
+
+def any_phrase($video_game)
+  catch_phrase = "I think I\'m inside of #{video_game}!"
+  puts catch_phrase
+end
